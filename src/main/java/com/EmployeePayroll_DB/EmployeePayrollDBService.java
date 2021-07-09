704c011e971074958365037999db02291c8938a7
Rekha-15 <rekhapatil.1509@gmail.com>
2021-07-09 00:51:10 -0400

    added : Reverting data : .

--- src/main/java/com/EmployeePayroll_DB/EmployeePayrollDBService.java
+++ src/main/java/com/EmployeePayroll_DB/EmployeePayrollDBService.java
@@ -5,11 +5,15 @@
  * @since 24/06/2021
  ************************************************************************************************/
 package com.EmployeePayroll_DB;
+
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Enumeration;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import model.EmployeePayrollData;
 
 /**
  * steps to follow to connect Java application with database 
@@ -22,50 +26,56 @@ import java.util.Enumeration;
  * 7) Close resources
  */
 
-public class EmployeePayrollDBService {
-	/**
-	 * main method
-	 * Printing welcome message
-	 * 
-	 * @param args
-	 */
+public class  EmployeePayrollDBService {
 
-	public static void main(String[] args) {
-	
-		System.out.println("Welcome to JDBC Employee Payroll Service program to perform various operations in database");
-		
-		{
-		
-		/**
-		 * Connecting program to database
-		 */
-		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
-		String username = "root";
-		String password = "root15";
-		Connection connection;
-		try {
-			System.out.println("connecting to database");
-			connection = DriverManager.getConnection(jdbcURL, username, password);
-			System.out.println("Connected Successfully!");
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("Driver Loaded");
-		} catch (SQLException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		listDrivers();
-	}
-	}
+    private Connection getConnection() throws SQLException {
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String username = "root";
+        String password = "root15";
+        System.out.println("connecting to database");
+        Connection connection = DriverManager.getConnection(jdbcURL,username,password);
+        System.out.println("Connected Successfully!");
+        return connection;
+    }
 
-	/**
-	 * list Drivers is a method 
-	 * In this method listing Driver used
-	 */
-
-	private static void listDrivers() {
-		Enumeration<Driver> driverList = DriverManager.getDrivers();
-		while (driverList.hasMoreElements()) {
-			Driver driverClass = (Driver) driverList.nextElement();
-			System.out.println(" " + driverClass.getClass().getName());
-		}
-	}
+    /**
+     * readsata method we will select table query.
+     * @return employeePayrollList
+     */
+    
+    public ArrayList<EmployeePayrollData> readData() {
+        String sql = "SELECT * FROM employee_payroll";
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
+        try {
+            Connection connection = this.getConnection();
+            Statement  statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                double salary = resultSet.getDouble("salary");
+                LocalDate startDate = resultSet.getDate("start").toLocalDate();
+                employeePayrollList.add(new EmployeePayrollData(id, name, salary, startDate));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employeePayrollList;
+    }
+        /*public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Loaded");
+        }catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        listDrivers();
+    }
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println(" "+driverClass.getClass().getName());
+        }
+    }*/
 }
--- src/test/java/com/EmployeePayroll_DB/EmployeePayrollServiceTest.java
+++ src/test/java/com/EmployeePayroll_DB/EmployeePayrollServiceTest.java
@@ -1,5 +1,34 @@
+/********************************************************************************************
+ * @Purpose JDBC Employee Payroll Service program to perform various operations in database. 
+ * Testing class
+ * @author Rekha
+ * @version 1.0
+ * @since 24/06/2021
+ ************************************************************************************************/
 package com.EmployeePayroll_DB;
 
+import org.junit.Assert;
+import org.junit.Test;
+import model.EmployeePayrollData;
+import java.util.List;
+/**
+ * EmployeePayrollServiceTest is a testing
+ * Testing for both true and false scenario
+ * @author rekha
+ *
+ */
 public class EmployeePayrollServiceTest {
-
+	@Test
+	public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+		EmployeePayrollDBService employeePayrollDBService = new EmployeePayrollDBService();
+		List<EmployeePayrollData> employeePayrollDataList = employeePayrollDBService.readData();
+		Assert.assertEquals(3, employeePayrollDataList.size());
+	}
+	
+	@Test
+	public void givenEmployeePayrollInDB_WhenRetrieved_ShouldnotMatchEmployeeCount() {
+		EmployeePayrollDBService employeePayrollDBService = new EmployeePayrollDBService();
+		List<EmployeePayrollData> employeePayrollDataList = employeePayrollDBService.readData();
+		Assert.assertNotSame(4, employeePayrollDataList.size());
+	}
 }
